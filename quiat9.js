f42c72f5789c4b233d6a5c333d1f0407fdb412be
gimenor <111014857+gimenor@users.noreply.github.com>
2022-08-22 20:30:00 +0200

    Update quiat9.js

--- quiat9.js
+++ quiat9.js
@@ -184,8 +184,8 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				'<br/>Pulse la tecla <b>I</b> con la derecha para los ítems que pertenezcan a la categoría <font color="#0000ff">rightAttribute</font>.<br/><br/>' +
 				'Si comete un error, aparecerá una <font color="#ff0000"><b>X</b></font> de color rojo. ' +
 				'Pulse la otra tecla para continuar.<br/>' +
-				'<u>Intente ir lo más rápido que pueda</u>, pero evitando errores.<br/><br/></p>'+
-				'<p align="center">Pulse la barra <b>espaciadora</b> cuando quiera empezar.</font></p></div>',
+				'<u>Intente ir rápido</u>, pero evitando errores.<br/><br/></p>'+
+				'<p align="center">Pulse <b>espacio</b> para empezar.</font></p></div>',
 			instAttributePracticeTouch: [
 				'<div>',
 					'<p align="center">',
@@ -209,10 +209,10 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				'Pulse la tecla <b>E</b> con la izquierda para los ítems que pertenezcan a la categoría <font color="#336600">leftCategory</font>. ' +
 				'<br/>Pulse la tecla <b>I</b> con la derecha para los ítems que pertenezcan a la categoría <font color="#336600">rightCategory</font>.<br/>' +
 				'Los ítems aparecerán de uno en uno.<br/><br/>' +
-				'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
-				'Press the other key to continue.<br/>' +
-				'<u>Go as fast as you can</u> while being accurate.<br/><br/></p>'+
-				'<p align="center">Press the <b>space bar</b> when you are ready to start.</font></p></div>',
+				'Si comete un error, aparecerá una <font color="#ff0000"><b>X</b></font> de color rojo. ' +
+				'Pulse la otra tecla para continuar.<br/>' +
+				'<u>Intente ir rápido</u>, pero evitando errores.<br/><br/></p>'+
+				'<p align="center">Pulse <b>espacio</b> para empezar.</font></p></div>',
 			instCategoriesPracticeTouch: [
 				'<div>',
 					'<p align="center">',
@@ -238,7 +238,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				'Cada ítem pertenece a una sola categoría.<br/><br/>' +
 				'Si comete un error, aparecerá una <font color="#ff0000"><b>X</b></font> de color rojo. ' +
 				'Pulse la otra tecla para continuar.<br/>' + 
-				'<u>Intente ir lo más rápido que pueda</u>, intentando no cometer errores.<br/><br/></p>' +
+				'<u>Intente ir rápido</u>, evitando cometer errores.<br/><br/></p>' +
 				'<p align="center">Pulse <b>espacio</b> para empezar.</font></p></div>',
 			instFirstCombinedTouch:[
 				'<div>',
